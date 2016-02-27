5a7ae5e384c80aed35cf391af36cacb5eb937b6b
krjukOFF <k.krjukovs@gmail.com>
2016-02-27 21:03:38 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 #ln -s /home/box/web/etc/guniconr.conf /etc/gunicorn/test.conf
 #/etc/init.d/gunicorn restart
