ae3365902c285e8e608445b4959c8fa0374b32ed
krjukOFF <k.krjukovs@gmail.com>
2016-03-06 15:40:54 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^', include('qa.urls')),
-    url(r'^admin/', admin.site.urls),
-]
-
-#urlpatterns = patterns('',
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    #url(r'^(|ask/|login/|new/|popular/|signup/|question/<\d+>/)$', 'qa.views.test', name='test'),
+    url(r'^$', 'qa.views.test', name='ask'),
+    url(r'^ask/', 'qa.views.test', name='ask'),
+    url(r'^login/', 'qa.views.test', name='login'),
+    url(r'^new/', 'qa.views.test', name='new'),
+    url(r'^popular/', 'qa.views.test', name='popular'),
+    url(r'^signup/', 'qa.views.test', name='signup'),
+    url(r'^question/<\d+>/', 'qa.views.test', name='question'),
 
- #   url(r'^', include('qa.urls')),
-  #  url(r'^admin/', include(admin.site.urls)),
-#)
+    url(r'^admin/', include(admin.site.urls)),
+)
