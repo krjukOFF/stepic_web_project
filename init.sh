12f9970f210db73b148fca9c29456f587d172ef1
krjukOFF <k.krjukovs@gmail.com>
2016-03-06 15:47:25 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,10 @@
-#sudo rm /etc/nginx/sites-enabled/default
-#ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#/etc/init.d/nginx restart
-#ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn/test.conf
-#/etc/init.d/gunicorn restart
-#sudo gunicorn -b 0.0.0.0:8080 hello:application
 sudo rm /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-avilable/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_django.conf  /etc/gunicorn.d/test_django
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
+sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
+
+#sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_django.conf  /etc/gunicorn.d/test_django
+#sudo /etc/init.d/gunicorn restart
