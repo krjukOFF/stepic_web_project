1d6be8251eaa3730e85e9031ef18932e43abcb9a
krjukOFF <k.krjukovs@gmail.com>
2016-03-06 13:12:10 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
-sudo rm /etc/nginx/sites-enabled/default
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
-ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn/test.conf
-/etc/init.d/gunicorn restart
+#sudo rm /etc/nginx/sites-enabled/default
+#ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#/etc/init.d/nginx restart
+#ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn/test.conf
+#/etc/init.d/gunicorn restart
 #sudo gunicorn -b 0.0.0.0:8080 hello:application
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-avilable/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_django.conf  /etc/gunicorn.d/test_django
+sudo /etc/init.d/gunicorn restart
