cffa7477310f3cca7283327278f9425e18e295d0
krjukOFF <k.krjukovs@gmail.com>
2016-03-06 10:47:17 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo rm /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 #ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn/test.conf
 #/etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:application
+#sudo gunicorn -b 0.0.0.0:8080 hello:application
