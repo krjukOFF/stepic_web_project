fa1b3f02435b68aeee92c67efaa2181330756d1d
krjukOFF <k.krjukovs@gmail.com>
2016-03-06 15:33:06 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
+    url(r'^', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+#urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include('qa.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+ #   url(r'^', include('qa.urls')),
+  #  url(r'^admin/', include(admin.site.urls)),
+#)
