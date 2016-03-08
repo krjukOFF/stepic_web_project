0341b6af035c719c022c4d96ad7e08171c34b95f
krjukOFF <k.krjukovs@gmail.com>
2016-03-08 14:18:28 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
 sudo ln -s /home/box/web/etc/django.conf /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
+mysql -uroot -e "CREATE DATABASE stepic_qa;"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_qa.* TO 'box'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
