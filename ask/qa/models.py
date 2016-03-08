3375c24d7e67f9e298915f4d0bb7d5aba627aa39
krjukOFF <k.krjukovs@gmail.com>
2016-03-08 07:33:44 +0200

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+        title=models.CharField(max_length=255)
+        text=models.TextField()
+        added_at=models.DateTimeField(blank=True)
+        rating=models.IntegerField()
+        author=models.CharField(max_length=255)
+        Likes=models.ManyToManyField(User)
+        def __unicode__(self):
+                return self.title
+class Answer(models.Model):
+        text=models.TextField()
+        added_at=models.DateTimeField(blank=True)
+        question=models.ForeignKey(Question)
+        author=models.CharField(max_length=255)
+        def __unicode__(self):
+                return self.title
