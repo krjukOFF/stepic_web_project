f502015ed4c7cb0ac68b67041dd8f73e015369b2
krjukOFF <k.krjukovs@gmail.com>
2016-03-08 14:09:47 +0200

    Create urls.py

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from .views import test
+from django.conf.urls import url
+
+
+urlpatterns = [
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/([0-9]+)/$', test),
+    url(r'^ask/\w*/ $', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
+]
